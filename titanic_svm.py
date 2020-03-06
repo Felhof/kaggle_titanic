5664e2ec44d982ba50ba6ba7188ed4813dcce960
Felix <fh4417@ic.ac.uk>
2020-03-06 20:35:27 +0000

    upsample data before fitting model

--- titanic_svm.py
+++ titanic_svm.py
@@ -2,11 +2,23 @@ from sklearn.metrics import accuracy_score, classification_report, confusion_mat
 from sklearn.model_selection import cross_val_score, cross_val_predict, KFold, train_test_split
 from sklearn.preprocessing import LabelBinarizer, MinMaxScaler
 from sklearn.svm import SVC
+from sklearn.utils import resample
 
 import numpy as np
 import pandas as pd
 
 numeric = ["Pclass","Age","SibSp","Parch","Fare"]
+
+def upsample(data, feature, minority=1):
+    data_majority = data[data[feature]==1-minority]
+    data_minority = data[data[feature]==minority]
+
+    data_minority_upsampled = resample(data_minority,
+                                        replace=True,
+                                        n_samples=len(data_majority))
+
+    data_upsampled = pd.concat([data_majority, data_minority])
+    return data_upsampled
 
 def preprocess(data_raw):
     data_raw.drop(["PassengerId", "Name", "Ticket", "Cabin"], axis="columns", inplace=True)
@@ -34,6 +46,7 @@ if __name__ == "__main__":
     train_df=pd.read_csv("train.csv")
     test_df=pd.read_csv("test.csv")
 
+    train_df = upsample(train_df, "Survived")
     train_df = preprocess(train_df)
 
     features = train_df.drop("Survived", axis="columns")
@@ -47,7 +60,9 @@ if __name__ == "__main__":
     kfold = KFold(n_splits=10) # k=10, split the data into 10 equal parts
     y_pred = cross_val_predict(model,features,target,cv=10)
     confusion = confusion_matrix(target,y_pred)
-    cunfusion_df = pd.DataFrame({" " : ["Died","Survived"], "Died" : confusion[:,0], "Survived" : confusion[:,1]})
+    died_ratios = [round(x, 3) for x in confusion[:,0] / sum(confusion[0])]
+    survived_ratios = [round(x, 3) for x in confusion[:,1] / sum(confusion[1])]
+    cunfusion_df = pd.DataFrame({" " : ["Died","Survived"], "Died" : died_ratios, "Survived" : survived_ratios})
     print("-------------Model Evaluation-------------------")
     print()
     print("Confusion matrix:")
