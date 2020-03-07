1b574ddc6402ac63ee01cfdd2ebec10c52d924f7
Felix <fh4417@ic.ac.uk>
2020-03-07 14:12:47 +0000

    perform hyperparametersearch

--- titanic_svm.py
+++ titanic_svm.py
@@ -1,13 +1,15 @@
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-from sklearn.model_selection import cross_val_score, cross_val_predict, KFold, train_test_split
+from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.preprocessing import LabelBinarizer, MinMaxScaler
 from sklearn.svm import SVC
 from sklearn.utils import resample
 
 import numpy as np
 import pandas as pd
+import sys
 
 numeric = ["Pclass","Age","SibSp","Parch","Fare"]
+
 
 def upsample(data, feature, minority=1):
     data_majority = data[data[feature]==1-minority]
@@ -19,6 +21,7 @@ def upsample(data, feature, minority=1):
 
     data_upsampled = pd.concat([data_majority, data_minority])
     return data_upsampled
+
 
 def preprocess(data_raw):
     data_raw.drop(["PassengerId", "Name", "Ticket", "Cabin"], axis="columns", inplace=True)
@@ -40,6 +43,33 @@ def preprocess(data_raw):
 
     return data_raw
 
+def hyperparmeter_search(X_train, y_train):
+    parameters = [
+        {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+        {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf','poly']}
+    ]
+    svc = SVC()
+    clf = GridSearchCV(svc, parameters)
+    clf.fit(X_train, y_train)
+    return clf.best_params_
+
+
+
+def evaluate(model, features, target):
+    y_pred = model.predict(features)
+    confusion = confusion_matrix(target,y_pred)
+    died_ratios = confusion[0,:] / sum(confusion[0])
+    survived_ratios = confusion[1,:] / sum(confusion[1])
+    ratios = np.vstack((died_ratios, survived_ratios))
+    confusion_df = pd.DataFrame({" " : ["Died","Survived"], "Died" : ratios[:,0], "Survived" : ratios[:,1]})
+    print("-------------Model Evaluation-------------------")
+    print()
+    print("Confusion matrix:")
+    print()
+    print(confusion_df.to_string(index=False))
+    print()
+    print(classification_report(target, y_pred, target_names=["Died", "Survived"]))
+
 
 if __name__ == "__main__":
 
@@ -54,19 +84,15 @@ if __name__ == "__main__":
 
     X_train, X_test, y_train, y_test = train_test_split(features, target)
 
-    model = SVC()
-    model.fit(X_train, y_train)
+    if "hps" in sys.argv[1:]:
+        best_params = hyperparmeter_search(X_train, y_train)
+        print("Found best hyperparameters:")
+        print(best_params)
+        best_svc = SVC(C=best_params["C"],kernel=best_params["kernel"], gamma=best_params.get("gamma", 'auto'))
+    else:
+        print("Using default parameters: C=10, linear kernel")
+        best_svc = SVC(C=10,kernel="linear")
 
-    kfold = KFold(n_splits=10) # k=10, split the data into 10 equal parts
-    y_pred = cross_val_predict(model,features,target,cv=10)
-    confusion = confusion_matrix(target,y_pred)
-    died_ratios = [round(x, 3) for x in confusion[:,0] / sum(confusion[0])]
-    survived_ratios = [round(x, 3) for x in confusion[:,1] / sum(confusion[1])]
-    cunfusion_df = pd.DataFrame({" " : ["Died","Survived"], "Died" : died_ratios, "Survived" : survived_ratios})
-    print("-------------Model Evaluation-------------------")
-    print()
-    print("Confusion matrix:")
-    print()
-    print(cunfusion_df.to_string(index=False))
-    print()
-    print(classification_report(target, y_pred, target_names=["Died", "Survived"]))
+    best_svc.fit(X_train, y_train)
+
+    evaluate(best_svc, X_test, y_test)
